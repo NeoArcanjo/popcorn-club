1106cd1e0d35951be952558205d96901d7d99ef9
NeoArcanjo <44139615+NeoArcanjo@users.noreply.github.com>
2021-10-23 14:38:44 -0300

    Iniciando pagina individual dos flmes

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from main import app
-print(app)
 
 if __name__ == '__main__':
     app.run()
--- routes.py
+++ routes.py
@@ -4,18 +4,10 @@ import random
 from functions import get_data, base_url, img_url, login_required
 from main import app, oauth
 
-@app.route("/login", methods=["POST", "GET"])
-def login():
-  # if form is submited
-    networks = get_data('networks/list?')
-    print(networks)
 
-    if request.method == "POST":
-        # record the user name
-        session["name"] = request.form.get("name")
-        # redirect to the main page
-        return redirect("/")
-    return render_template("login.html")
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 @app.route('/login/google')
 def google():
@@ -23,17 +15,21 @@ def google():
     redirect_uri = url_for('authorize', _external=True)
     return google.authorize_redirect(redirect_uri)
 
+
 @app.route('/login/callback')
 def authorize():
     google = oauth.create_client('google')  # create the google oauth client
-    token = google.authorize_access_token()  # Access token from google (needed to get user info)
-    resp = google.get('userinfo')  # userinfo contains stuff u specificed in the scrope
+    # Access token from google (needed to get user info)
+    token = google.authorize_access_token()
+    # userinfo contains stuff u specificed in the scrope
+    resp = google.get('userinfo')
     user_info = resp.json()
     user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
     # Here you use the profile/user data that you got and query your database find/register the user
     # and set ur own data in the session not the profile from google
     session['profile'] = user_info
-    session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
+    # make the session permanant so it keeps existing after broweser gets closed
+    session.permanent = True
     return redirect('/')
 
 
@@ -43,20 +39,24 @@ def logout():
         session.pop(key)
     return redirect('/')
 
+
 @app.route("/search")
 @login_required
 def search():
     search = request.args.get('q')
-    results = get_data(f"search/multi?language=en-US&page=1&include_adult=false&query={search}&")
+    results = get_data(
+        f"search/multi?language=en-US&page=1&include_adult=false&query={search}&")
     results = results["results"]
     set_movie = results[0]
 
     return render_template("search_result.html",  set_movie=set_movie, img_url=img_url, results=results)
 
+
 @app.route('/')
 @login_required
 def index():
-    popular = get_data('discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&')
+    popular = get_data(
+        'discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&')
     # nacionais = get_data('discover/movie?certification_country=BR&certification.lte=G&sort_by=popularity.desc&')
     popular = popular["results"]
     secure_random = random.SystemRandom()
@@ -70,9 +70,10 @@ def index():
 
     return render_template('index.html', set_movie=set_movie, img_url=img_url, popular=popular, trend=trend, netflix=netflix)
 
-@app.route('/filmes')
+
+@app.route('/movie')
 @login_required
-def filmes():
+def movie():
     genres = get_data('genre/movie/list?')
     genres = genres["genres"]
 
@@ -80,7 +81,8 @@ def filmes():
         results = get_data(f'discover/movie?&with_genres={genre["id"]}&')
         genre["results"] = results["results"]
 
-    popular = get_data('discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&')
+    popular = get_data(
+        'discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&')
     popular = popular["results"]
     secure_random = random.SystemRandom()
     set_movie = secure_random.choice(popular)
@@ -93,6 +95,7 @@ def filmes():
 
     return render_template('filmes.html', set_movie=set_movie, img_url=img_url, genres=genres, popular=popular, trend=trend, netflix=netflix)
 
+
 @app.route('/series')
 @login_required
 def series():
@@ -103,7 +106,8 @@ def series():
         results = get_data(f'discover/tv?&with_genres={genre["id"]}&')
         genre["results"] = results["results"]
 
-    popular = get_data('discover/tv?certification_country=US&certification.lte=G&sort_by=popularity.desc&')
+    popular = get_data(
+        'discover/tv?certification_country=US&certification.lte=G&sort_by=popularity.desc&')
     popular = popular["results"]
     secure_random = random.SystemRandom()
     set_show = secure_random.choice(popular)
@@ -117,7 +121,14 @@ def series():
 
     return render_template('series.html', set_show=set_show, img_url=img_url, genres=genres, popular=popular, trend=trend, netflix=netflix)
 
+
+@app.route("/<type>/about/<id>")
+@login_required
+def about(type, id):
+    movie = get_data(f'{type}/{id}?')
+    return render_template('sobre.html', movie=movie, img_url=img_url)
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('not_found.html')
-

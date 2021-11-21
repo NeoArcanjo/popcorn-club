d2f0a6c29b904b25e71f0946cae9880f8d3457f2
NeoArcanjo <44139615+NeoArcanjo@users.noreply.github.com>
2021-11-21 17:59:40 -0300

    Update dashboard_assets.py

--- popcorn_club/Dashboard/dashboard_assets.py
+++ popcorn_club/Dashboard/dashboard_assets.py
@@ -4,5 +4,5 @@ from flask_assets import Bundle
 dashboard_style_bundle = Bundle(
     'dashboard_bp/src/css/*.css',
     filters='cssmin',
-    output='dist/css/dashboard.css'
+    output='dist/css/dashboard.min.css'
 )
